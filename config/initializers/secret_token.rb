588cc2f42fe25c769051fc9d28c8aedbb399fa2d
Tim Savolainen <timsavolainen@me.com>
2014-10-05 14:22:01 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mollyterest::Application.config.secret_key_base = '505d1a40bad9e307421687ef646fb835616442d02a2ee448a12c22889c6b5e873abef4193edc5368e99932f66b7c9b3853c4f5642fe444bfd4845955fdfaf14b'
